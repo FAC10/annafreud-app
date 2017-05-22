019fffceb0a5ac318fd39f1b2f7707560d1434f6
lucyrose93 <lucyrose@protonmail.com>
2017-05-22 22:12:22 +0100

    refine layout on landing page
    added profile pic menu in header. relates #4

---
+++ landing.js
@@ -0,0 +1,6 @@
+var headerMenu = document.querySelector('.header_menu_items');
+var headerProfilePic = document.querySelector('.header_profile_pic');
+
+headerProfilePic.addEventListener('click', () => {
+  headerMenu.style.visibility = headerMenu.style.visibility === 'hidden' ? 'visible' : 'hidden'
+})
