33693cd85e2a551b3019a81d33be9da17dd81944
Maja Kudlicka <maja.kudlicka@gmail.com>
2017-05-22 18:59:22 +0100

    fixed description not disappearing on login page

--- login.js
+++ login.js
@@ -3,9 +3,13 @@ var login_info= document.querySelector('.login_info');
 
 // info_text.style.display = info_button.style.display === 'none'? '' : 'block';
 
-info_button.addEventListener('click', () => {
+info_button.addEventListener('mouseover', () => {
 
-  login_info.style.display = 'block';
-
+if (login_info.style.display== 'block'){
+  login_info.style.display = 'none';
+}
+  else {
+    login_info.style.display = 'block';
+}
 
 })
