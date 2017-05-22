e4a3a177a110f2923da5b389cb5551e8cfa5c370
Maja Kudlicka <maja.kudlicka@gmail.com>
2017-05-22 17:00:42 +0100

    added styling to the login page

---
+++ login.js
@@ -0,0 +1,11 @@
+var info_button =document.querySelector('.info_button');
+var login_info= document.querySelector('.login_info');
+
+// info_text.style.display = info_button.style.display === 'none'? '' : 'block';
+
+info_button.addEventListener('click', () => {
+
+  login_info.style.display = 'block';
+
+
+})
