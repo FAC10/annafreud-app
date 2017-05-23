4b6ca420579464a07f427e2a93aced58566603c6
Maja Kudlicka <maja.kudlicka@gmail.com>
2017-05-23 11:23:58 +0100

    added success pages
    relates to #33

--- login.js
+++ login.js
@@ -1,7 +1,15 @@
 var info_button =document.querySelector('.info_button');
 var login_info= document.querySelector('.login_info');
 
+// info_text.style.display = info_button.style.display === 'none'? '' : 'block';
 
-info_button.addEventListener('click', () => {
-  login_info.style.display = login_info.style.display === 'none' ? 'block' : 'none';
+info_button.addEventListener('mouseover', () => {
+
+if (login_info.style.display== 'block'){
+  login_info.style.display = 'none';
+}
+  else {
+    login_info.style.display = 'block';
+}
+
 })
