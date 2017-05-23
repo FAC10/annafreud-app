33f8228cac5776ce74a42f3b954c719b4485ae1f
Maja Kudlicka <maja.kudlicka@gmail.com>
2017-05-23 15:10:27 +0100

    added functionality to hide and unhide a question depeneding on user input
    relates to #5

--- logameal.js
+++ logameal.js
@@ -1,2 +1,14 @@
 var time = document.getElementById("current_time_input")
+var no = document.getElementById("no");
+var yes = document.getElementById("yes");
 time.value = new Date().toString().slice(4,-15);
+
+no.addEventListener('click', ()=>{
+  if (no.checked === true) {
+  document.getElementById("what_happened").style.display = 'block';
+}
+});
+
+yes. addEventListener('click', ()=>{
+  document.getElementById("what_happened").style.display= 'none';
+});
