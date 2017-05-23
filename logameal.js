83fdf24b05af6231705004a516551cfbb976415d
Maja Kudlicka <maja.kudlicka@gmail.com>
2017-05-23 14:17:12 +0100

    updated questions on food log

--- logameal.js
+++ logameal.js
@@ -1,7 +1,2 @@
 var time = document.getElementById("current_time_input")
 time.value = new Date().toString().slice(4,-15);
-time.value.slice(0,-15);
-
-// current_time_input.addEventListener('click', ()=> {
-//   current_time_input.value = new Date();
-// })
