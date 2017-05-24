35ec7d8ce383800bb1f768985c62433d36b5945d
lucyrose93 <lucyrose@protonmail.com>
2017-05-24 14:45:36 +0100

    add calendar page hidden info
    keeping it minimal but intuitive with a qn mark symbol to explain how it works. Therapy days are highlighted in squares.

--- js/calendar.js
+++ js/calendar.js
@@ -4,3 +4,10 @@ var headerProfilePic = document.querySelector('.header_profile_pic');
 headerProfilePic.addEventListener('click', () => {
   headerMenu.style.display = headerMenu.style.display === 'none' ? 'inline-block' : 'none'
 })
+
+var infoButton = document.querySelector('.info_button');
+var calendarText = document.querySelector('.calendar_text');
+
+infoButton.addEventListener('click', () => {
+  calendarText.style.display = calendarText.style.display === 'none' ? 'inline-block' : 'none'
+})
