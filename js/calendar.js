0ee1aca56e5fb698a8e1a8e591180a09464a91e0
Maja Kudlicka <maja.kudlicka@gmail.com>
2017-05-25 11:01:49 +0100

    added functionality to select buttons in select mode
    relates to #16

--- js/calendar.js
+++ js/calendar.js
@@ -11,3 +11,29 @@ var calendarText = document.querySelector('.calendar_text');
 infoButton.addEventListener('click', () => {
   calendarText.style.display = calendarText.style.display === 'none' ? 'inline-block' : 'none'
 })
+
+var select_mode = false;
+
+document.getElementById("select_button").addEventListener('click', () => {
+  console.log(select_mode);
+if (select_mode === false) {
+  var tds = document.getElementsByTagName('td');
+
+  for (var i =0; i < tds.length; i++) {
+    var j= i+1;
+    tds[i].innerHTML = "<a href=#>"+j+"</a>"
+    tds[i].addEventListener('click', (i)=>{
+      i.target.style.color = 'yellow';
+    });
+  }
+}
+if (select_mode === true) {
+  location.reload();
+  select_mode = false
+}
+else {
+  select_mode = true;
+}
+
+
+});
