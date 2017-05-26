0b5b7f2903e267b9fff2637f73855829c03d1340
Maja Kudlicka <maja.kudlicka@gmail.com>
2017-05-26 13:00:27 +0100

    added most fiels from the food diary to the local storage
    relates to #118

--- js/logameal.js
+++ js/logameal.js
@@ -5,6 +5,7 @@ var q1 = document.getElementById("q1");
 var q2 = document.getElementById("q2");
 var q3 = document.getElementById("q3");
 var q4 = document.getElementById("q4");
+var q5 = document.getElementById("q5");
 var submit_button = document.getElementById("submit_button");
 time.value = new Date().toString().slice(15,-15);
 
@@ -14,12 +15,25 @@ submit_button.addEventListener('click', ()=> {
   localStorage.setItem('q2', q2.value);
   localStorage.setItem('q3', q3.value);
   localStorage.setItem('q4', q4.value);
-  var answer1 = localStorage.getItem('q1');
+  localStorage.setItem('q5', q5.value);
+  localStorage.setItem('time', time.value);
+
+  if (yes.checked === true) {
+    localStorage.setItem('q6', 'yes');
+  }
+
+  else {
+    localStorage.setItem('q6', 'no');
+  }
+  var food_description = localStorage.getItem('q1');
   var subjective_binge = localStorage.getItem('q2');
   var objective_binge= localStorage.getItem('q3');
-  var answer4 = localStorage.getItem('q4');
-  console.log(answer1, answer2, answer3, answer4);
-  console.log('success');
+  var location_and_setting = localStorage.getItem('q4');
+  var thoughts_and_feelings = localStorage.getItem('q5');
+  var keep_it_down = localStorage.getItem('q6');
+  var timelog = localStorage.getItem('time');
+  console.log(food_description, subjective_binge, objective_binge, location_and_setting, thoughts_and_feelings, keep_it_down, timelog);
+
 });
 
 
